062cf368ca50ed17e350121e248754e7cf0a4265
tgroeneboer <tom.groeneboer@liones.nl>
2016-11-21 11:47:26 +0100

    Add repro case

---
+++ ConsoleApp1/CustomXmlResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    public class CustomXmlResolver : XmlResolver
+    {
+	    public override object GetEntity(Uri absoluteUri, string role, Type ofObjectToReturn)
+	    {
+			// Custom stuff here
+		    throw new NotImplementedException();
+	    }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Saxon.Api;
+
+namespace ConsoleApp1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var processor = new Processor();
+			var compiler = processor.NewXsltCompiler();
+			var resolver = new CustomXmlResolver();
+
+			// This setter doesn't work properly
+			compiler.XmlResolver = resolver;
+			
+			var shouldBeTrue = ReferenceEquals(compiler.XmlResolver, resolver);
+		}
+	}
+}
